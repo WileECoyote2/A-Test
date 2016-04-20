3ab824b53117974f4f1468d3cf1b88caeca969d7
JakeyCouper <WileECoyote2@users.noreply.github.com>
2016-04-20 10:15:54 -0700

    following player tutorial, added in a LOT of assets.

---
+++ Assets/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidBody;
+	int floorMask;
+}
