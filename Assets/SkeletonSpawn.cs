3084bc090ab0428022aa2fbe6f8d4f0cad37a02b
JakeyCouper <WileECoyote2@users.noreply.github.com>
2016-04-27 10:19:19 -0700

    building platforms

---
+++ Assets/SkeletonSpawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkeletonSpawn : MonoBehaviour {
+
+
+	public GameObject SkeletonPrefab;
+
+	public float delay = 10f;
+	public float TimeBetweenSpawn;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SkeletonGenerator());
+
+	}
+	IEnumerator SkeletonGenerator(){
+		yield return new WaitForSeconds (delay);
+
+		if (TimeBetweenSpawn == delay){
+			Instantiate (SkeletonPrefab);
+			TimeBetweenSpawn = 0;
+		}
+		else {
+			TimeBetweenSpawn += Time.deltaTime;
+		}
+	}
+	
+
+}
